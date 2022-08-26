33ed4cad7aa45aa3215aa498e1afa59e43ea8c79
oManhattan <matheusbatuque@gmail.com>
2022-08-26 20:23:04 -0300

    .

---
+++ src/main/java/com/pi/ProjetoIntegrador004Application.java
@@ -0,0 +1,13 @@
+package com.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador004Application.class, args);
+	}
+
+}

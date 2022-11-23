1234d6c17315e27075650480bed2a7fda446b9c8
oManhattan <matheusbatuque@gmail.com>
2022-11-23 02:30:00 -0300

    request para navegador com cookie salvo

--- src/main/java/com/pi/controller/PaginasController.java
+++ src/main/java/com/pi/controller/PaginasController.java
@@ -1,27 +1,56 @@
 package com.pi.controller;
 
+import java.util.Arrays;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pi.logic.util.JWTUtil;
+
 @CrossOrigin(origins = "*", exposedHeaders = "*")
 @Controller
 public class PaginasController {
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @GetMapping("/")
     public ModelAndView login() {
         return new ModelAndView("login");
     }
-    
+
+    @GetMapping("/usuario-logado")
+    public ModelAndView usuarioLogado(HttpServletRequest request) throws Exception {
+        String authorization = "";
+        Cookie authorizationCookie = Arrays.asList(request.getCookies()).stream().filter((cookie) -> cookie.getName().equals("Authorization")).findFirst().orElse(null);
+        if (authorizationCookie != null) {
+            authorization = authorizationCookie.getValue().replace("$", " ");
+        }
+        String formattedToken = jwtUtil.formatToken(authorization);
+        String customerType = jwtUtil.getUserAuthorityFromToken(formattedToken);
+        if (customerType.equals("ROLE_PROFISSIONAL")) {
+            return new ModelAndView("profissional");
+        } else if (customerType.equals("ROLE_CLIENTE")) {
+            return new ModelAndView("cliente");
+        } else {
+            return new ModelAndView("falhaNaVerificacao");
+        }
+    }
+
     @GetMapping("/tela-profissional")
     public ModelAndView tela_profissional() {
         return new ModelAndView("profissional");
     }
-    
+
     @GetMapping("/tela-cliente")
     public ModelAndView tela_cliente() {
         return new ModelAndView("cliente");
     }
-    
+
 }

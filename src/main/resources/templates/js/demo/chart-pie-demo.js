85064bad2a068c57a54a002d4964d9a805ae8e9b
gtozeti <gtozeti@gmail.com>
2022-11-23 00:14:29 -0300

    Front final 2 . Será ?

---
+++ src/main/resources/templates/js/demo/chart-pie-demo.js
@@ -0,0 +1,33 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Massa muscular", "Gordura"],
+    datasets: [{
+      data: [55, 30],
+      backgroundColor: ['#e24b11', '#e2bc11', '#14213D']
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
